733eef7d57b9b346e10da80e841622631a6310b2
Игорь Иванов <is.ivanov@bars.group>
2023-10-08 23:57:40 +0300

    Add migrate script

--- app/views.py
+++ app/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    is_auth = request.session.get('is_auth', False)
+    context = {'is_auth': is_auth}
+    return render(request, 'index.html', context)
+
+def register(request):
+    context = {'is_auth': True}
+    return render(request, 'index.html', context)
