4e8fa2ca7792072c9e23dc8c4070db7a03ca5c92
Valerianjones1 <dinkiev.valery@yandex.ru>
2023-10-10 15:34:57 +0300

    Fix views.py

--- app/views.py
+++ app/views.py
@@ -71,7 +71,6 @@ def login_view(request):
     username = request.POST["username"]
     password = request.POST["password"]
     user = authenticate(request, username=username, password=password)
-    print(user, username, password)
     if user is not None:
         login(request, user)
         context = {'is_auth': True}
