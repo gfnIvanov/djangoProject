b9f394ead8a6bdd60d2c2dfa788a14c3b7f746da
Игорь Иванов <is.ivanov@bars.group>
2023-10-10 23:06:40 +0300

    login

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=450)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, )
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     body = models.TextField()
 
     def str(self):
--- app/views.py
+++ app/views.py
@@ -1,29 +1,88 @@
-from django.contrib.auth import logout, authenticate, login
-from django.shortcuts import render, redirect
+from typing import Optional
+from django.contrib.auth import authenticate
+from django.shortcuts import render
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpRequest, HttpResponseRedirect
 from django.contrib.auth.models import User
-from .forms import RegisterForm, PostForm
+from .forms import RegisterForm, LoginForm, PostForm
 from .models import Post
 
 
-def index(request):
-    is_auth = request.session.get('is_auth', False)
-    context = {'is_auth': is_auth}
+def index(request: HttpRequest):
+    context = {'active_page': 'index'}
+    auth_data = request.session.get('auth_data', {'is_auth': False})
+    context.update(auth_data)
     return render(request, 'index.html', context)
 
 
-def home(request):
-    # Это для вкладки "Главная". Чтобы все посты выводились
-    if request.user.is_authenticated():
-        posts = Post.objects.all()
-        return render(request, 'index.html', {"posts": posts})
-    else:
-        redirect('app')
+def posts(request: HttpRequest):
+    context = {'active_page': 'posts'}
+    auth_data = request.session.get('auth_data', {'is_auth': False})
+    context.update(auth_data)
+    return render(request, 'posts.html', context)
 
 
-def create_post(request):
-    context = {'show_register': False, 'is_auth': True}
+def register(request: HttpRequest):
+    """
+    Регистрация нового пользователя
+    При успешной регистрации стразу выполняем авторизацию пользователя
+    """  
+    context = {'show_register': True}
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            try:
+                User.objects.get(username=form.cleaned_data['user_login'])
+                context['form_data'] = form.cleaned_data
+                context['errors'] = {'user_login': [{'message': 'Указанный логин уже присутствует в базе'}]}
+                return render(request, 'index.html', context)
+            except User.DoesNotExist:
+                user = User.objects.create_user(username=form.cleaned_data['user_login'],
+                                                password=form.cleaned_data['user_password'])
+                user.first_name = form.cleaned_data['user_firstname']
+                user.last_name = form.cleaned_data['user_surname']
+                user.save()
+                _set_auth_data(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        context['form_data'] = form.cleaned_data
+        context['errors'] = form.errors.get_json_data()
+    return render(request, 'index.html', context)
+
+
+def login(request: HttpRequest):
+    """
+    Авторизация пользователя
+    """
+    context = {'show_login': True}
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['user_login']
+            password = form.cleaned_data['user_password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                _set_auth_data(request, User.objects.get(username=username))
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                context['errors'] = {'auth_failed': [{'message': 'Не удалось пройти авторизацию'}]}
+        context['form_data'] = form.cleaned_data
+        if context.get('errors') is not None:
+            context['errors'].update(form.errors.get_json_data()) 
+        else:
+            context['errors'] = form.errors.get_json_data()
+    return render(request, 'index.html', context)
+
+
+def logout(request):
+    """
+    Выход из аккаунта
+    """
+    _set_auth_data(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+def create_post(request: HttpRequest):
+    context = {'show_register': False}
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -44,46 +103,11 @@ def create_post(request):
     return render(request, 'index.html', context)
 
 
-def register(request):
-    context = {'show_register': True}
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            try:
-                User.objects.get(username=form.cleaned_data['user_login'])
-                context['form_data'] = form.cleaned_data
-                context['errors'] = {'user_login': [{'message': 'Указанный логин уже присутствует в базе'}]}
-                return render(request, 'index.html', context)
-            except User.DoesNotExist:
-                user = User.objects.create_user(username=form.cleaned_data['user_login'],
-                                                password=form.cleaned_data['user_password'])
-                user.first_name = form.cleaned_data['user_firstname']
-                user.last_name = form.cleaned_data['user_surname']
-                user.save()
-                request.session['is_auth'] = True
-                return HttpResponseRedirect(reverse('index'))
-        context['form_data'] = form.cleaned_data
-        context['errors'] = form.errors.get_json_data()
-    return render(request, 'index.html', context)
-
-
-def login_view(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        context = {'is_auth': True}
-        return HttpResponseRedirect(reverse('index'))
-
-    else:
-        return HttpResponseRedirect(reverse('index'))
-
-
-def logout_view(request):
-    # TODO Нужно подправить файл headers.html, чтобы отправлять через кнопку POST-запрос
-    # if request.method == "POST":
-    logout(request)
-    context = {'is_auth': False}
-    return render(request, "index.html", context)
-# return render(request, 'index.html')
+def _set_auth_data(request: HttpRequest, user: Optional[User] = None) -> None:
+    if user is None:
+        request.session.clear()
+        return
+    request.session['auth_data'] = dict.fromkeys(['is_auth', 'firstname', 'surname'])
+    request.session['auth_data']['is_auth'] = True
+    request.session['auth_data']['firstname'] = user.first_name
+    request.session['auth_data']['surname'] = user.last_name[0] + '.'
